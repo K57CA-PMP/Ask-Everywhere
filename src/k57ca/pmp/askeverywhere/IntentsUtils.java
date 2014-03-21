5890e5affc360190eaebb2114a95b888dac9edb2
tuantq57 <tuantq.vnu@gmail.com>
2014-03-21 16:57:02 +0700

    [#3] add simple listView

---
+++ src/k57ca/pmp/askeverywhere/IntentsUtils.java
@@ -0,0 +1,18 @@
+package k57ca.pmp.askeverywhere;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+
+public class IntentsUtils {
+	public static void newsfeed(Activity activity) {
+		Intent intent = new Intent(activity, Newsfeed.class);
+		activity.startActivity(intent);
+	}
+	
+	public static void invokeWebBrowser(Activity activity){ 
+		Intent intent = new Intent(Intent.ACTION_VIEW); 
+		intent.setData(Uri.parse("http://www.google.com.vn")); 
+		activity.startActivity(intent); 
+	}
+}

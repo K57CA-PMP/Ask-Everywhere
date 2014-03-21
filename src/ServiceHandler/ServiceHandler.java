74d9e30f8fca5a18c2e91f547bd619e55af384ec
tuantq57 <tuantq.vnu@gmail.com>
2014-03-22 00:24:09 +0700

    [#3] succeed in JSON tut

---
+++ src/ServiceHandler/ServiceHandler.java
@@ -0,0 +1,5 @@
+package ServiceHandler;
+
+public class ServiceHandler {
+
+}
--- src/k57ca/pmp/askeverywhere/MainActivity.java
+++ src/k57ca/pmp/askeverywhere/MainActivity.java
@@ -1,69 +1,201 @@
 package k57ca.pmp.askeverywhere;
 
-import android.app.Activity;
-import android.app.Fragment;
+import java.util.ArrayList;
+import java.util.HashMap;
+ 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+ 
+public class MainActivity extends ListActivity {
+ 
+    private ProgressDialog pDialog;
+ 
+    // URL to get contacts JSON
+    private static String url = "http://api.androidhive.info/contacts/";
+ 
+    // JSON Node names
+    private static final String TAG_CONTACTS = "contacts";
+    private static final String TAG_ID = "id";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_EMAIL = "email";
+    private static final String TAG_ADDRESS = "address";
+    private static final String TAG_GENDER = "gender";
+    private static final String TAG_PHONE = "phone";
+    private static final String TAG_PHONE_MOBILE = "mobile";
+    private static final String TAG_PHONE_HOME = "home";
+    private static final String TAG_PHONE_OFFICE = "office";
+ 
+    // contacts JSONArray
+    JSONArray contacts = null;
+ 
+    // Hashmap for ListView
+    ArrayList<HashMap<String, String>> contactList;
+ 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+ 
+        contactList = new ArrayList<HashMap<String, String>>();
+ 
+        ListView lv = getListView();
+ 
+        // Listview on item click listener
+        lv.setOnItemClickListener(new OnItemClickListener() {
+ 
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                    int position, long id) {
+                // getting values from selected ListItem
+                String name = ((TextView) view.findViewById(R.id.name))
+                        .getText().toString();
+                String cost = ((TextView) view.findViewById(R.id.email))
+                        .getText().toString();
+                String description = ((TextView) view.findViewById(R.id.mobile))
+                        .getText().toString();
+ 
+            }
+        });
+ 
+        // Calling async task to get json
+        new GetContacts().execute();
+    }
+ 
+    /**
+     * Async task class to get json by making HTTP call
+     * */
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+ 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+ 
+        }
+ 
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // Creating service handler class instance
+            ServiceHandler sh = new ServiceHandler();
+ 
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url, ServiceHandler.GET);
+ 
+            Log.d("Response: ", "> " + jsonStr);
+ 
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                     
+                    // Getting JSON Array node
+                    contacts = jsonObj.getJSONArray(TAG_CONTACTS);
+ 
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                         
+                        String id = c.getString(TAG_ID);
+                        String name = c.getString(TAG_NAME);
+                        String email = c.getString(TAG_EMAIL);
+                        String address = c.getString(TAG_ADDRESS);
+                        String gender = c.getString(TAG_GENDER);
+ 
+                        // Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject(TAG_PHONE);
+                        String mobile = phone.getString(TAG_PHONE_MOBILE);
+                        String home = phone.getString(TAG_PHONE_HOME);
+                        String office = phone.getString(TAG_PHONE_OFFICE);
+ 
+                        // tmp hashmap for single contact
+                        HashMap<String, String> contact = new HashMap<String, String>();
+ 
+                        // adding each child node to HashMap key => value
+                        contact.put(TAG_ID, id);
+                        contact.put(TAG_NAME, name);
+                        contact.put(TAG_EMAIL, email);
+                        contact.put(TAG_PHONE_MOBILE, mobile);
+ 
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                Log.e("ServiceHandler", "Couldn't get any data from the url");
+            }
+ 
+            return null;
+        }
+ 
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this, contactList,
+                    R.layout.list_item, new String[] { TAG_NAME, TAG_EMAIL,
+                            TAG_PHONE_MOBILE }, new int[] { R.id.name,
+                            R.id.email, R.id.mobile });
+ 
+            setListAdapter(adapter);
+        }
+ 
+    }
+ 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
 		int base = Menu.FIRST;
-		MenuItem newsfeed = menu.add(base, 1, 1, "Newsfeed");
-		MenuItem search = menu.add(base, 2, 2, "Google");
-		return true;
-	}
+		MenuItem search = menu.add(base, 1, 1, "Newsfeed");
+//		MenuItem addQuestion = menu.add(base, 2, 2, "Add question");
+//		MenuItem help = menu.add(base, 3, 3, "Help");
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		switch (item.getItemId()) {
-		case 1:
-			IntentsUtils.newsfeed(this);
-			break;
-		case 2:
-			IntentsUtils.invokeWebBrowser(this);
-			break;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-		return true;
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+        case 1:
+        	IntentsUtils.newsfeed(this);
+        	break;
+        case 2:
+        	break;
+        case 3:
+        	break;
+        default:
+        	return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+    
 }

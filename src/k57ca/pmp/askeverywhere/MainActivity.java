6e07b16ddad033bb3d0fcce871e5afab5922d674
halink0803 <halink0803@gmail.com>
2014-03-21 18:03:58 +0700

    [#2] Create log in with google

--- src/k57ca/pmp/askeverywhere/MainActivity.java
+++ src/k57ca/pmp/askeverywhere/MainActivity.java
@@ -3,12 +3,15 @@ package k57ca.pmp.askeverywhere;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class MainActivity extends Activity {
@@ -16,13 +19,16 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
+		setContentView(R.layout.activity_main);			
+			
+		if (savedInstanceState != null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
+		} else
+			{
+				startActivity(new Intent(this, LogInActivity.class));
+			}
+	}	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {

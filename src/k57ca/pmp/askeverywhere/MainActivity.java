0972fe23553cdf5b6be8df6f492e204dd67a5bbc
=tienhoangna <=tienhoangna@gmail.com>
2014-03-22 13:46:51 +0700

    built insert entities

--- src/k57ca/pmp/askeverywhere/MainActivity.java
+++ src/k57ca/pmp/askeverywhere/MainActivity.java
@@ -1,64 +1,60 @@
 package k57ca.pmp.askeverywhere;
 
+/* Copyright (c) 2012 Google Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+import k57ca.pmp.askeverywhere.userendpoint.model.User;
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+
+
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+ 
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+   super.onCreate(savedInstanceState);
+   setContentView(R.layout.activity_main);
+   
+   User hoang = new User();
+   hoang.setEmail("tienhoangna@gmail.com");
+   hoang.setName("Hoang");
+   hoang.setUserId("H");
+   new AddUserToBackEnd(hoang).execute();
+   
+   User giap = new User();
+   giap.setEmail("giap@gmail.com");
+   giap.setName("Giap");
+   giap.setUserId("G");
+   new AddUserToBackEnd(giap).execute();
+   
 
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
+ }
+ 
+ @Override
+ public boolean onCreateOptionsMenu(Menu menu) {
+   // Inflate the menu; this adds items to the action bar if it is present.
+   getMenuInflater().inflate(R.menu.main, menu);
+   return true;
+ }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
+ /**
+  * AsyncTask for calling Mobile Assistant API for checking into a place (e.g., a store)
+  */
 
 }
